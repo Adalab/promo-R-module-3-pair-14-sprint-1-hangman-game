62533543f428d2bd19f7cd35917bec638d275377
Rpg87 <sra.picadillo@gmail.com>
2022-11-22 16:20:58 +0100

    we paint the dummy

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
+import { useState } from 'react';
 import '../styles/App.scss';
 
 
 function App() {
+  const [numberOfErrors, setNumberOfErrors] = useState(0);
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    setNumberOfErrors(numberOfErrors + 1);
+
+  }
   return (
     <div className="page">
       <header>
@@ -35,18 +43,19 @@ function App() {
             </ul>
           </div>
           <form className="form">
-            <label className="title" for="last-letter">Escribe una letra:</label>
+            <label className="title" htmlFor="last-letter">Escribe una letra:</label>
             <input
-              autocomplete="off"
+              // autocomplete="off"
               className="form__input"
-              maxlength="1"
+              // maxlength="1"
               type="text"
               name="last-letter"
               id="last-letter"
             />
+            <button onClick={handleClick}>Incrementar</button>
           </form>
         </section>
-        <section className="dummy error-5">
+        <section className={`dummy error-${numberOfErrors}`}>
           <span className="error-13 eye"></span>
           <span className="error-12 eye"></span>
           <span className="error-11 line"></span>
